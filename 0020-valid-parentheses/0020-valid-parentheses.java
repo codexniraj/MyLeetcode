4f23e352d77537ffbb240eb357a4b97615277c39
Niraj Yogesh Wani <106158322+codexniraj@users.noreply.github.com>
2023-11-02 20:10:40 +0530

    Time: 1 ms (98.89%), Space: 40.1 MB (94.46%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isValid(String s) {
+      Stack < Character > stack = new Stack < > ();
+        for (char a: s.toCharArray()) {
+            if (a == '(' || a == '[' || a == '{') stack.push(a);
+            else if (stack.empty()) return false;
+            else if (
+                (a == ']' && stack.pop() != '[') || 
+                (a == ')' && stack.pop() != '(') || 
+                (a == '}' && stack.pop() != '{')
+                ) return false;
+        }
+        return stack.empty();   
+    }
+}
