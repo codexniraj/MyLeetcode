29cd37258b9e727dae4cc89998b96e0df9b79b27
Niraj Yogesh Wani <106158322+codexniraj@users.noreply.github.com>
2023-11-11 10:40:17 +0530

    Time: 1 ms (100.00%), Space: 41.4 MB (37.02%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int myAtoi(String s) {
+     int i = 0;
+        Boolean isPos = true;
+
+        while(i < s.length() && s.charAt(i) == ' ')
+            i++;
+
+        if(i == s.length())
+            return 0;
+
+        if(s.charAt(i) == '-'){
+            isPos = false;
+            i++;
+        }
+        else if(s.charAt(i) == '+')
+            i++;
+
+        long ans = 0;
+
+        for(; i < s.length(); i++){
+            if(s.charAt(i) < '0' || s.charAt(i) > '9')
+                break;
+
+            ans = ans*10 + s.charAt(i)-'0';
+
+            if(isPos && ans > (Math.pow(2, 31)-1)){
+                ans = (long)(Math.pow(2, 31)-1);
+                break;
+            }
+            else if(!isPos && ans > Math.pow(2, 31)){
+                ans = (long)Math.pow(2, 31);
+                break;
+            }
+        }
+
+        if(!isPos)
+            return (int)(-1*ans);
+
+        return (int)ans;   
+    }
+}
